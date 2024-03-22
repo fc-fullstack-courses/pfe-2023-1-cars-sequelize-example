65a9935c1ce319b518e88297aadd6f03161c6ec9
Kyrylo <fresh.js.ftd.@gmail.com>
2024-03-22 21:18:43 +0200

    Add file path constant

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = require('./router');
+const CONSTANTS = require('./constants');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(CONSTANTS.FILE_PATH));
 app.use(router);
 
 module.exports = app;
---
+++ constants.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const CONSTANTS = {
+  FILE_PATH: path.resolve(__dirname, 'public')
+}
+
+module.exports = CONSTANTS;
